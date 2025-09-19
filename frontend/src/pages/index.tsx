76784714a876f6cc1d7bfb53bed3b6fe70a670ac
DuskStarGITHUB <contactspeencer@gmail.com>
2025-09-19 14:47:49 -0600

    style default

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,14 @@
+// DEPENDENCIES
 import { Head } from "minista"
 import Example from "@components/Example"
 
+// WEB RENDER BASE "/"
 export default function () {
   return (
     <>
+      {/* METADATA */}
       <Head>
-        <title>Hello!</title>
+        <title>TEMPLATE VITE V3</title>
         <link rel="icon" href="/img/icon.png" />
         <link rel="stylesheet" href="/css/index.css" />
       </Head>
